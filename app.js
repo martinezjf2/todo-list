6f19515635836abd4fdec8a0ca76fca1ea5936ee
github username <github email address>
2020-12-05 16:21:57 -0500

    add gitignore procfile and updat ports

--- app.js
+++ app.js
@@ -150,6 +150,13 @@ app.get("/about", function(req, res){
 })
 
 
-app.listen(3000, function(){
-console.log('You are listening to Port:3000');
+let port = process.env.PORT
+if (port == null || port == "") {
+    port = 3000
+}
+
+app.listen(port)
+
+app.listen(port, function(){
+console.log('Server has started successfully!');
 });
